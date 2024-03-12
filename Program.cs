d43212a9c993c08297e6829a2fc4aa5bebe52b4e
Fin157 <444stuart@gmail.com>
2024-03-12 18:58:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EmailDuplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new(/*your input file path here*/);
+            using StreamWriter sw = new(/*your output file path here*/);
+
+            HashSet<string> seenEmails = [];
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                if (seenEmails.Contains(line))
+                    continue;
+
+                seenEmails.Add(line);
+                sw.WriteLine(line);
+            }
+        }
+    }
+}
